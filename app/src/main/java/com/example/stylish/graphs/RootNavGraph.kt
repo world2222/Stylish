a596d7f6775a4fcbbc26da56d33e2dc64d5b6d95
unknown <world2222222222@gmail.com>
2023-11-13 01:37:03 +0900

    FEAT: Add new app icon and navigation bar
    Added the new app icon and also put navigation bar that allows the users
    to navigate to home, create, search, and mypage screen.

---
+++ app/src/main/java/com/example/stylish/graphs/RootNavGraph.kt
@@ -0,0 +1,25 @@
+package com.example.stylish.graphs
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.example.stylish.ui.screens.main.MainScreen
+
+@Composable
+fun RootNavGraph(navController: NavHostController) {
+    NavHost(
+        navController = navController,
+        route = RootGraph.LoginScreen.route,
+        startDestination = RootGraph.MainScreen.route,
+    ) {
+        composable(route = RootGraph.MainScreen.route) {
+            MainScreen()
+        }
+    }
+}
+
+sealed class RootGraph(val route: String) {
+    data object LoginScreen: RootGraph(route = "LoginScreen")
+    data object MainScreen: RootGraph(route = "MainScreen")
+}
