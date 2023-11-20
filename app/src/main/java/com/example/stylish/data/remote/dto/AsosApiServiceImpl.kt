b8b4f52d865e3e3a3b7b45c2c4b262e502145646
world2222 <world2222@hanmail.net>
2023-11-21 03:26:26 +0900

    FEAT: Added service and imple for api call
    Added AsosApiService file and AsosApiServiceImple file for api calls.
    Still need to setup more!

---
+++ app/src/main/java/com/example/stylish/data/remote/dto/AsosApiServiceImpl.kt
@@ -0,0 +1,9 @@
+package com.example.stylish.data.remote.dto
+
+import com.example.stylish.data.remote.dto.category.CategoryItem
+
+class AsosApiServiceImpl : AsosApiService {
+    override suspend fun getCategory(): List<CategoryItem> {
+        TODO("Not yet implemented")
+    }
+}
